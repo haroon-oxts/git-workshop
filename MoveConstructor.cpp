a7375fcf0a753e82a412e2e566ff22b2f39d85d3
Haroon Haider <HHAIDER@oxts.com>
2020-04-14 12:20:18 +0100

    Changing tabs

--- MoveConstructor.cpp
+++ MoveConstructor.cpp
@@ -1,15 +1,16 @@
 // Rule of 5 and Move constructor example
 // We will use char* here on purpose (instead of say string)
-
 // Also, this example DRYs the code as much as possible. Can you spot where?
 
 include "X.cpp"
 
-X retByVal() {
-        return X("Hello World");
+X retByVal() 
+{
+    return X("Hello World");
+    UNICODE
 }
 
-int main() {
-        X x4(retByVal());
-        return 0;
+int main() 
+{
+    x4(retByVal());
 }
